cd1d14b0f420b22ad538bb8513fa00a181312619
bagl0025 <bagl0025@umn.edu>
2022-05-18 17:51:32 -0500

    added subtraction, everything working - needs styleing

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,39 +36,68 @@ var addEm = function(player, dartNum, addSub, idClicked) {
     if (addSub === 1) {
         //player 1 (left)
         if (player === 'l') {
-            // if 3 darts have been counted time to score
+            // if all 3 darts haven't been hit keep couting darts
             if (dartLeft[dartNum] < 3) {
                 dartLeft[dartNum]++;
                 $('#'+player+'d'+dartNum).text(dartLeft[dartNum]);
             }
-            // if all 3 darts haven't been hit keep couting darts
+            // if 3 darts have been counted time to score
             else {
                 scoreLeft = scoreLeft + dartNum;
-                console.log(scoreLeft)
+                dartLeft[dartNum]++;
                 $('#lScore').text(scoreLeft);
             }
         }
         //player 2 (right)
         else if (player === 'r') {
-            // if 3 darts have been counted time to score
+            // if all 3 darts haven't been hit keep couting darts
             if (dartRight[dartNum] < 3) {
                 dartRight[dartNum]++;
                 $('#'+player+'d'+dartNum).text(dartRight[dartNum]);
             }
-            // if all 3 darts haven't been hit keep couting darts
+            // if 3 darts have been counted time to score
             else {
                 scoreRight = scoreRight + dartNum;
+                dartRight[dartNum]++;
                 $('#rScore').text(scoreRight);
+            }
+        }
+    }
+    // if addsub == 0 (subtract) applies to darts only
+    // can't sub after scoring begins
+    else {
+        if (player === 'l') {
+            if (dartLeft[dartNum] > 3) {
+                alert("Can't subtract darts after scoring begins!");
+                return false;
+            }
+            else {
+                if (dartLeft[dartNum] === 0) {
+                    return false;
+                }
+                else {
+                    dartLeft[dartNum]--;
+                    $('#'+player+'d'+dartNum).text(dartLeft[dartNum]);
+                }
+            }
+        }
+        else {
+            if (dartRight[dartNum] > 3) {
+                alert("Can't subtract darts after scoring begins!");
+                return false;
+            }
+            else {  
+                if (dartRight[dartNum] === 0) {
+                    return false;
+                } 
+                else {
+                    dartRight[dartNum]--;
+                    $('#'+player+'d'+dartNum).text(dartRight[dartNum]);
+                }
             }
         }
     }
 };
 
-//need to add subtract functionality - how?
-//points vs darts
-//??? if scoring reduce score
-//if counting darts reduce dart count
-
 // need to replace numbers with icons
-
 //finish styling
