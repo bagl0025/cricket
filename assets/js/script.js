7b8bdf8e6acaba13c952dd8278cf2d1c56ec28cb
bagl0025 <bagl0025@umn.edu>
2022-06-13 15:25:16 -0500

    fix bugs, refactor ifs

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,97 +10,98 @@ $('button').click(function (event) {
   //get id
   var idClicked = event.target.id;
 
-  if (idClicked === 'help') {
-    const helpMsg =
-      'Use the + button to toggle between add / subtract.\n' +
-      'It applies to darts and points.\n\n' +
-      'The green numbers add darts or points.\n\n' +
-      'The yellow refresh button starts a new game.\n\n' +
-      'The blue circle with a question mark loads this help screen.\n';
-    swal({
-      title: 'Help',
-      text: helpMsg,
-      button: 'Close',
-    });
-    return;
+  switch (idClicked) {
+    case 'help':
+      help();
+      break;
+    case 'refresh':
+      location.reload();
+      break;
+    case 'addsub':
+      if (addSub === 1) {
+        addSub = 0;
+        $('#addsub').css('background-color', 'red');
+        $('#addsub').removeClass('fa-solid fa-plus');
+        $('#addsub').addClass('fa-solid fa-minus');
+      } else {
+        addSub = 1;
+        $('#addsub').css('background-color', 'green');
+        $('#addsub').addClass('fa-solid fa-plus');
+      }
+      break;
+    default:
+      //separate letter and number from idClicked
+      player = idClicked.replace(/[0-9]/g, '');
+      dartNum = idClicked.replace(/\D/g, '');
+      if (addSub === 1) {
+        addEm(player, dartNum);
+      } else if (addSub === 0) {
+        subEm(player, dartNum);
+      }
   }
-  if (idClicked === 'addsub') {
-    if (addSub === 1) {
-      addSub = 0;
-      $('#addsub').css('background-color', 'red');
-      $('#addsub').removeClass('fa-solid fa-plus');
-      $('#addsub').addClass('fa-solid fa-minus');
-    } else {
-      addSub = 1;
-      $('#addsub').css('background-color', 'green');
-      $('#addsub').addClass('fa-solid fa-plus');
-    }
-  } else {
-    //separate letter and number from idClicked
-    player = idClicked.replace(/[0-9]/g, '');
-    dartNum = idClicked.replace(/\D/g, '');
-    addEm(player, dartNum, addSub);
-  }
-});
+}); //on 'button' click function
 
-var addEm = function (player, dartNum, addSub) {
+var addEm = function (player, dartNum) {
   // need to convert dartnum to INT since it was obtained from string
   dartNum = parseInt(dartNum);
-  if (addSub === 1) {
-    //player 1 (left)
-    if (player === 'l') {
-      // if all 3 darts haven't been hit keep couting darts
-      if (dartLeft[dartNum] < 3) {
-        if (dartLeft[dartNum] === 0) {
-          $('#' + player + 'd' + dartNum)
-            .addClass('fa-solid fa-slash fa-3x')
-            .text('');
-        } else if (dartLeft[dartNum] === 1) {
-          $('#' + player + 'd' + dartNum)
-            .removeClass('fa-solid fa-slash')
-            .addClass('fa-solid fa-xmark fa-3x');
-        } else if (dartLeft[dartNum] === 2) {
-          $('#' + player + 'd' + dartNum)
-            .removeClass('fa-solid fa-xmark')
-            .addClass('fa-solid fa-circle-radiation fa-3x');
-        }
-        dartLeft[dartNum]++;
+  // player 1 left
+  if (player === 'l') {
+    // if all 3 darts haven't been hit keep couting darts
+    if (dartLeft[dartNum] < 3) {
+      if (dartLeft[dartNum] === 0) {
+        $('#' + player + 'd' + dartNum)
+          .addClass('fa-solid fa-slash fa-3x')
+          .text('');
+      } else if (dartLeft[dartNum] === 1) {
+        $('#' + player + 'd' + dartNum)
+          .removeClass('fa-solid fa-slash')
+          .addClass('fa-solid fa-xmark fa-3x');
+      } else if (dartLeft[dartNum] === 2) {
+        $('#' + player + 'd' + dartNum)
+          .removeClass('fa-solid fa-xmark')
+          .addClass('fa-solid fa-circle-radiation fa-3x');
       }
-      // if 3 darts have been counted time to score
-      else {
-        scoreLeft = scoreLeft + dartNum;
-        dartLeft[dartNum]++;
-        $('#lScore').text(scoreLeft);
-      }
+      dartLeft[dartNum]++;
     }
-    //player 2 (right)
-    else if (player === 'r') {
-      // if all 3 darts haven't been hit keep couting darts
-      if (dartRight[dartNum] < 3) {
-        if (dartRight[dartNum] === 0) {
-          $('#' + player + 'd' + dartNum)
-            .addClass('fa-solid fa-slash fa-3x')
-            .text('');
-        } else if (dartRight[dartNum] === 1) {
-          $('#' + player + 'd' + dartNum)
-            .removeClass('fa-solid fa-slash')
-            .addClass('fa-solid fa-xmark fa-3x');
-        } else if (dartRight[dartNum] === 2) {
-          $('#' + player + 'd' + dartNum)
-            .removeClass('fa-solid fa-xmark')
-            .addClass('fa-solid fa-circle-radiation fa-3x');
-        }
-        dartRight[dartNum]++;
-      }
-      // if 3 darts have been counted time to score
-      else {
-        scoreRight = scoreRight + dartNum;
-        dartRight[dartNum]++;
-        $('#rScore').text(scoreRight);
-      }
+    // if 3 darts have been counted time to score
+    else {
+      scoreLeft = scoreLeft + dartNum;
+      dartLeft[dartNum]++;
+      $('#lScore').text(scoreLeft);
     }
-    // subtract section - allows you to deduct points and darts
-  } else if (addSub === 0) {
+  } //end player 1
+  //player 2 (right)
+  else if (player === 'r') {
+    // if all 3 darts haven't been hit keep couting darts
+    if (dartRight[dartNum] < 3) {
+      if (dartRight[dartNum] === 0) {
+        $('#' + player + 'd' + dartNum)
+          .addClass('fa-solid fa-slash fa-3x')
+          .text('');
+      } else if (dartRight[dartNum] === 1) {
+        $('#' + player + 'd' + dartNum)
+          .removeClass('fa-solid fa-slash')
+          .addClass('fa-solid fa-xmark fa-3x');
+      } else if (dartRight[dartNum] === 2) {
+        $('#' + player + 'd' + dartNum)
+          .removeClass('fa-solid fa-xmark')
+          .addClass('fa-solid fa-circle-radiation fa-3x');
+      }
+      dartRight[dartNum]++;
+    }
+    // if 3 darts have been counted time to score
+    else {
+      scoreRight = scoreRight + dartNum;
+      dartRight[dartNum]++;
+      $('#rScore').text(scoreRight);
+    }
+  } //end player 2
+}; //end addem
+
+// subtract section - allows you to deduct points and darts
+var subEm = function (player, dartNum) {
+  dartNum = parseInt(dartNum);
+  if (player === 'l') {
     if (dartLeft[dartNum] === 1) {
       $('#' + player + 'd' + dartNum)
         .removeClass('fa-solid fa-slash fa-3x')
@@ -121,6 +122,8 @@ var addEm = function (player, dartNum, addSub) {
       dartLeft[dartNum]--;
       $('#lScore').text(scoreLeft);
     }
+  } // end player 1
+  else if (player === 'r') {
     if (dartRight[dartNum] === 1) {
       $('#' + player + 'd' + dartNum)
         .removeClass('fa-solid fa-slash fa-3x')
@@ -141,9 +144,20 @@ var addEm = function (player, dartNum, addSub) {
       dartRight[dartNum]--;
       $('#rScore').text(scoreRight);
     }
-  }
-}; //end addem
+  } //end player 2
+}; //end subem
 
-$('#refresh').click(function () {
-  location.reload();
-});
+var help = function () {
+  const helpMsg =
+    'Use the + button to toggle between add / subtract.\n' +
+    'It applies to darts and points.\n\n' +
+    'The green numbers add darts or points.\n\n' +
+    'The yellow refresh button starts a new game.\n\n' +
+    'The blue circle with a question mark loads this help screen.\n';
+  swal({
+    title: 'Help',
+    text: helpMsg,
+    button: 'Close',
+  });
+  return;
+};
